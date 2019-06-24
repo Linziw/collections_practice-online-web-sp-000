a9d3e8cb0c11c930bc12bdedecfa3b2394b8eb93
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-24 19:29:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,10 +13,10 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-    temp = a
-    a = b
-    b = temp
-    return a,b
+    temp = array[1]
+    array[1] = array[2]
+    array[2] = temp
+    array
 end
 
 
