d86830ccc6186f99db94f1c744949f7111f190f5
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-24 19:25:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
+def sort_array_asc(int_array)
+ return int_array.sort!
+end
+
+def sort_array_desc(array)
+ return array.sort!{|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort! {|a,b| 
+    a.length <=> b.length}
+  return array
+end
+
+
 
